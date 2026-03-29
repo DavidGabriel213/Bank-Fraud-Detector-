43fd62d9ed1eb590b19ac0a96965ccac6808dae6
David Gabriel <gdkimati@gmail.com>
2026-03-29 08:55:47 +0100

    Remove unused pandas import
    Removed unused pandas import from APP.py

--- APP.py
+++ APP.py
@@ -1,7 +1,6 @@
 from flask import Flask,render_template, request
 import os
 import numpy as np
-import pandas as pd
 import pickle
 app=Flask(__name__) 
 model=pickle.load(open("best_model.pkl","rb"))
